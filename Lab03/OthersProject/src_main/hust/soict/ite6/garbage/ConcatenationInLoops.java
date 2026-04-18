34315b709148bb162f0a1a0a52368b7d7ec088d0
ThinhHoang22 <hoangngocthinh2201@gmail.com>
2026-04-18 17:53:01 +0700

    String StringBufer StringBuilder

---
+++ Lab03/OthersProject/src_main/hust/soict/ite6/garbage/ConcatenationInLoops.java
@@ -0,0 +1,39 @@
+package hust.soict.ite6.garbage;
+
+import java.util.Random;
+
+public class ConcatenationInLoops {
+    public static void main(String[] args) {
+        Random r = new Random(123);
+
+        // Use String
+        long start = System.currentTimeMillis();
+        String s = "";
+        for (int i = 0; i < 65536; i++) {
+            s += r.nextInt(2);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Time using String: " + (end - start) + " ms");
+
+        // Use StringBuilder
+        r = new Random(123);
+        start = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 65536; i++) {
+            sb.append(r.nextInt(2));
+        }
+        s = sb.toString();
+        System.out.println("Time using StringBuilder: " + (end - start) + " ms, length = " + s.length());
+
+        // Use StringBuffer
+        r = new Random(123);
+        start = System.currentTimeMillis();
+        StringBuffer sf = new StringBuffer();
+        for (int i = 0; i < 65536; i++) {
+            sf.append(r.nextInt(2));
+        }
+        s = sf.toString();
+        end = System.currentTimeMillis();
+        System.out.println("Time using StringBuffer: " + (end - start) + " ms");
+    }
+}
